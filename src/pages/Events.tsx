3068a2f1fd552d39657cd5656d404e4c74f0ffff
Chetan302003 <141828783+Chetan302003@users.noreply.github.com>
2026-01-15 23:14:13 +0530

    Update Events.tsx
    spelling mistake, it was events not Events

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -32,7 +32,7 @@ interface TruckersMPEvent {
 }
 
 const Events = () => {
-  const [Events, setEvents] = useState<TruckersMPEvent[]>([]);
+  const [events, setEvents] = useState<TruckersMPEvent[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [attendingEvents, setAttendingEvents] = useState<TruckersMPEvent[]>([]);
